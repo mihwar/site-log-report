6228f1671c81fd18d3ddee0a19b23eca9c79b0c8
Mikhail Dubrovniy <m.dubrovniy@cardpay.com>
2021-09-29 23:11:20 +1000

    New report made for 5xx errors and their URLs.

---
+++ report-5xx.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+path="/home/dubrovnyi/LOGS/"
+files=`ls $path | grep "^unlimint.com" | grep access`
+for name in $files
+do
+  zcat $path/$name | grep "Aug\/2021" | awk -f report-5xx.awk
+done
